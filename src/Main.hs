f70c4cf85b43e2a90389dedc238411d356d34567
Ruud van Asseldonk <dev@veniogames.com>
2018-10-27 13:35:16 +0200

    Define instructions and pretty-print them

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,9 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
+import Control.Monad (mapM_)
+import Data.List (intercalate)
+
 data Variable a where
   -- A variable is identified by its sequence number.
   Variable :: Int -> Variable a
@@ -22,36 +25,68 @@ data Value a where
   ValBool   :: Bool   -> Value Bool
 
 deriving instance Eq (Value a)
-deriving instance Show (Value a)
 
-data Operand a where
-  Var   :: Variable a -> Operand a
-  Const :: Value a    -> Operand a
+instance Show (Value a) where
+  show v = case v of
+    ValString str -> show str
+    ValInt i -> show i
+    ValBool b -> show b
 
-deriving instance Eq (Operand a)
-deriving instance Show (Operand a)
+data Term a where
+  Var   :: Variable a -> Term a
+  Const :: Value a    -> Term a
+
+deriving instance Eq (Term a)
+
+instance Show (Term a) where
+  show op = case op of
+    Var v   -> show v
+    Const c -> "\x1b[37m" ++ (show c) ++ "\x1b[0m"
 
 pattern CString str = Const (ValString str)
 pattern CInt i = Const (ValInt i)
 pattern CTrue = Const (ValBool True)
 pattern CFalse = Const (ValBool False)
 
-data Operation
-  = And [Operand Bool]
-  | Or [Operand Bool]
-  | Concat [Operand String]
-  | Add [Operand Int]
-  | Sub [Operand Int]
-  deriving (Eq, Show)
+data Expr a where
+  And    :: [Term Bool]   -> Expr Bool
+  Or     :: [Term Bool]   -> Expr Bool
+  Concat :: [Term String] -> Expr String
+  Add    :: [Term Int]    -> Expr Int
+  Sub    :: [Term Int]    -> Expr Int
+
+deriving instance Eq (Expr a)
+
+instance Show (Expr a) where
+  show op =
+    let
+      display prefix args = prefix ++ " " ++ (intercalate " " $ fmap show args)
+    in case op of
+      And args    -> display "and" args
+      Or args     -> display "or" args
+      Concat args -> display "concat" args
+      Add args    -> display "add" args
+      Sub args    -> display "sub" args
+
+data Instr where
+  Define :: Variable a -> Expr a -> Instr
+  Yield :: Instr
+
+--deriving instance Eq Instr
+
+instance Show Instr where
+  show instr = case instr of
+    Define var expr -> (show var) ++ " = " ++ (show expr)
+    Yield -> "yield"
 
 main :: IO ()
 main =
   let
     ops =
-      [ Add [CInt 0, CInt 1, CInt 2]
-      , And [CTrue, CTrue, Var (Variable 0)]
-      , Or [Var (Variable 0), Var (Variable 1), Var (Variable 2)]
-      , Add [Var (Variable 2), Var (Variable 3), Var (Variable 4)]
+      [ Define (Variable 0) (Add [CInt 0, CInt 1, CInt 2])
+      , Define (Variable 1) (And [CTrue, CTrue, Var (Variable 0)])
+      , Define (Variable 2) (Or [Var (Variable 0), Var (Variable 1), Var (Variable 2)])
+      , Define (Variable 3) (Add [Var (Variable 2), Var (Variable 3), Var (Variable 4)])
       ]
   in do
-    putStrLn $ show ops
+    mapM_ (putStrLn . show) ops
