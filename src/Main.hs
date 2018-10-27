6aeeb9bca9b2756ad3cd9304449a0a7e58d83ac5
Ruud van Asseldonk <dev@veniogames.com>
2018-10-27 12:17:33 +0200

    Color variables when pretty-printing
    And choose the color based on the variable, so I can see at a glance
    which variables are the same ones. This matters even more because
    variables have no names apart from their sequence numbers.

--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,18 @@ data Variable a where
   deriving (Eq)
 
 instance Show (Variable a) where
-  show (Variable i) = '$' : show i
+  show (Variable i) =
+    let
+      -- Pick one ANSI color per variable, so we can look at the pretty printed
+      -- code and easily identify which variables are the same ones.
+      color = (i `mod` 6) + 1
+    in
+      "\x1b[3" ++ (show color) ++ "m$" ++ (show i) ++ "\x1b[0m"
 
 data Value a where
-  VString :: String -> Value String
-  VInt    :: Int    -> Value Int
-  VBool   :: Bool   -> Value Bool
+  ValString :: String -> Value String
+  ValInt    :: Int    -> Value Int
+  ValBool   :: Bool   -> Value Bool
 
 deriving instance Eq (Value a)
 deriving instance Show (Value a)
@@ -25,10 +31,10 @@ data Operand a where
 deriving instance Eq (Operand a)
 deriving instance Show (Operand a)
 
-pattern CString str = Const (VString str)
-pattern CInt i = Const (VInt i)
-pattern CTrue = Const (VBool True)
-pattern CFalse = Const (VBool False)
+pattern CString str = Const (ValString str)
+pattern CInt i = Const (ValInt i)
+pattern CTrue = Const (ValBool True)
+pattern CFalse = Const (ValBool False)
 
 data Operation
   = And [Operand Bool]
@@ -43,7 +49,9 @@ main =
   let
     ops =
       [ Add [CInt 0, CInt 1, CInt 2]
-      , And [CTrue, CTrue, CFalse]
+      , And [CTrue, CTrue, Var (Variable 0)]
+      , Or [Var (Variable 0), Var (Variable 1), Var (Variable 2)]
+      , Add [Var (Variable 2), Var (Variable 3), Var (Variable 4)]
       ]
   in do
     putStrLn $ show ops
