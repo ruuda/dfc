064668631216997d5b088209c316e06cad103640
Ruud van Asseldonk <dev@veniogames.com>
2018-10-27 18:43:55 +0200

    Extract types into separate module
    And make a small wrapper for bindings that keeps a fresh counter.

--- src/Main.hs
+++ src/Main.hs
@@ -1,120 +1,30 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE ExistentialQuantification #-}
-
 import Control.Monad (mapM_)
-import Data.IntMap (IntMap)
 import Data.List (intercalate)
 
-import qualified Data.IntMap as IntMap
-
-data Variable a where
-  -- A variable is identified by its sequence number.
-  Variable :: Int -> Variable a
-  deriving (Eq)
-
-instance Show (Variable a) where
-  show (Variable i) =
-    let
-      -- Pick one ANSI color per variable, so we can look at the pretty printed
-      -- code and easily identify which variables are the same ones.
-      color = (i `mod` 6) + 1
-    in
-      "\x1b[3" ++ (show color) ++ "m$" ++ (show i) ++ "\x1b[0m"
-
-data Value a where
-  ValString :: String -> Value String
-  ValInt    :: Int    -> Value Int
-  ValBool   :: Bool   -> Value Bool
-
-deriving instance Eq (Value a)
-
-instance Show (Value a) where
-  show v = case v of
-    ValString str -> show str
-    ValInt i -> show i
-    ValBool b -> show b
-
-data Term a where
-  Var   :: Variable a -> Term a
-  Const :: Value a    -> Term a
-
-deriving instance Eq (Term a)
-
-instance Show (Term a) where
-  show op = case op of
-    Var v   -> show v
-    Const c -> "\x1b[37m" ++ (show c) ++ "\x1b[0m"
+import Types (Variable, Value (..), Term (..), Field (..), Expr (..), Bindings, newBindings, bind)
 
 pattern CString str = Const (ValString str)
 pattern CInt i = Const (ValInt i)
 pattern CTrue = Const (ValBool True)
 pattern CFalse = Const (ValBool False)
 
--- A named field.
-data Field a where
-  Field :: String -> Field a
-
-deriving instance Eq (Field a)
-
-instance Show (Field a) where
-  show (Field name) = show name
-
-data Expr a where
-  Not    :: Term Bool     -> Expr Bool
-  And    :: [Term Bool]   -> Expr Bool
-  Or     :: [Term Bool]   -> Expr Bool
-  Concat :: [Term String] -> Expr String
-  Add    :: [Term Int]    -> Expr Int
-  Sub    :: [Term Int]    -> Expr Int
-  Load   :: Field a       -> Expr a
-  Select :: Term Bool -> Term a -> Term a -> Expr a
-
-deriving instance Eq (Expr a)
-
-instance Show (Expr a) where
-  show op =
-    let
-      display prefix args = prefix ++ " " ++ (intercalate " " $ fmap show args)
-    in case op of
-      Not arg     -> display "not" [arg]
-      And args    -> display "and" args
-      Or args     -> display "or" args
-      Concat args -> display "concat" args
-      Add args    -> display "add" args
-      Sub args    -> display "sub" args
-      Load field  -> display "load" [field]
-      Select cond vtrue vfalse -> "select " ++ (show cond) ++ " " ++ (show vtrue) ++ " " ++ (show vfalse)
-
-data Some f = forall a. Some (f a)
-
 data Program a b = Program
   { programInput :: Variable a
-  , programBindings :: !(IntMap (Some Expr))
+  , programBindings :: !Bindings
   , programCondition :: !(Term Bool)
   , programYield :: !(Term b)
   }
 
 instance Show (Program a b) where
-  show p =
-    let
-      showBinding :: (Int, Some Expr) -> String
-      showBinding (k, Some v) = (show $ Variable k) ++ " = " ++ (show v)
-      bindings = fmap showBinding $ IntMap.toList $ programBindings p
-    in
-      "input " ++ (show $ programInput p) ++ "\n" ++
-      intercalate "\n" bindings ++ "\n" ++
-      "yield " ++ (show $ programYield p) ++ " if " ++ (show $ programCondition p)
+  show p = intercalate "\n"
+    [ "input " ++ (show $ programInput p)
+    , show $ programBindings p
+    , "yield " ++ (show $ programYield p) ++ " if " ++ (show $ programCondition p)
+    ]
 
 data GenProgram i = GenProgram
   { genInput :: Variable i
-  , genFresh :: !Int
-  , genBindings :: !(IntMap (Some Expr))
+  , genBindings :: !Bindings
   , genCondition :: !(Term Bool)
   }
 
@@ -141,10 +51,10 @@ instance Monad (Gen i) where
 genProgram :: Gen a (Term b) -> Program a b
 genProgram gen =
   let
+    (input, bindings) = newBindings
     initial = GenProgram
-      { genInput = Variable 0
-      , genFresh = 1
-      , genBindings = IntMap.empty
+      { genInput = input
+      , genBindings = bindings
       , genCondition = CTrue
       }
     (finalValue, finalState) = runGen gen initial
@@ -159,14 +69,10 @@ genProgram gen =
 define :: Expr a -> Gen i (Term a)
 define expr = Gen $ \state ->
   let
-    k = genFresh state
-    bindings = genBindings state
-    state' = state
-      { genFresh = k + 1
-      , genBindings = IntMap.insert k (Some expr) bindings
-      }
+    (var, newBindings) = bind expr $ genBindings state
+    newState = state { genBindings = newBindings }
   in
-    (Var $ Variable k, state')
+    (Var var, newState)
 
 loadInput :: Gen i (Term i)
 loadInput = Gen $ \state -> (Var $ genInput state, state)
---
+++ src/Types.hs
@@ -0,0 +1,106 @@
+module Types
+  ( Variable -- Constructor not exposed to make Gen type safe.
+  , Value (..)
+  , Term (..)
+  , Field (..)
+  , Expr (..)
+  , Bindings
+  , newBindings
+  , bind
+  ) where
+
+import Data.List (intercalate)
+import Data.IntMap (IntMap)
+
+import qualified Data.IntMap as IntMap
+
+data Variable a where
+  -- A variable is identified by its sequence number.
+  Variable :: Int -> Variable a
+  deriving (Eq)
+
+instance Show (Variable a) where
+  show (Variable i) =
+    let
+      -- Pick one ANSI color per variable, so we can look at the pretty printed
+      -- code and easily identify which variables are the same ones.
+      color = (i `mod` 6) + 1
+    in
+      "\x1b[3" ++ (show color) ++ "m$" ++ (show i) ++ "\x1b[0m"
+
+data Value a where
+  ValString :: String -> Value String
+  ValInt    :: Int    -> Value Int
+  ValBool   :: Bool   -> Value Bool
+
+deriving instance Eq (Value a)
+
+instance Show (Value a) where
+  show v = case v of
+    ValString str -> show str
+    ValInt i -> show i
+    ValBool b -> show b
+
+data Term a where
+  Var   :: Variable a -> Term a
+  Const :: Value a    -> Term a
+
+deriving instance Eq (Term a)
+
+instance Show (Term a) where
+  show op = case op of
+    Var v   -> show v
+    Const c -> "\x1b[37m" ++ (show c) ++ "\x1b[0m"
+
+-- A named field.
+data Field a where
+  Field :: String -> Field a
+
+deriving instance Eq (Field a)
+
+instance Show (Field a) where
+  show (Field name) = show name
+
+data Expr a where
+  Not    :: Term Bool     -> Expr Bool
+  And    :: [Term Bool]   -> Expr Bool
+  Or     :: [Term Bool]   -> Expr Bool
+  Concat :: [Term String] -> Expr String
+  Add    :: [Term Int]    -> Expr Int
+  Sub    :: [Term Int]    -> Expr Int
+  Load   :: Field a       -> Expr a
+  Select :: Term Bool -> Term a -> Term a -> Expr a
+
+deriving instance Eq (Expr a)
+
+instance Show (Expr a) where
+  show op =
+    let
+      display prefix args = prefix ++ " " ++ (intercalate " " $ fmap show args)
+    in case op of
+      Not arg     -> display "not" [arg]
+      And args    -> display "and" args
+      Or args     -> display "or" args
+      Concat args -> display "concat" args
+      Add args    -> display "add" args
+      Sub args    -> display "sub" args
+      Load field  -> display "load" [field]
+      Select cond vtrue vfalse -> "select " ++ (show cond) ++ " " ++ (show vtrue) ++ " " ++ (show vfalse)
+
+data Some f = forall a. Some (f a)
+
+data Bindings = Bindings !Int !(IntMap (Some Expr))
+
+instance Show Bindings where
+  show (Bindings _ b) =
+    let
+      showBinding :: (Int, Some Expr) -> String
+      showBinding (k, Some v) = (show $ Variable k) ++ " = " ++ (show v)
+    in
+      intercalate "\n" $ fmap showBinding $ IntMap.toList b
+
+newBindings :: (Variable a, Bindings)
+newBindings = (Variable 0, Bindings 1 IntMap.empty)
+
+bind :: Expr a -> Bindings -> (Variable a, Bindings)
+bind expr (Bindings i b) = (Variable i, Bindings (i + 1) (IntMap.insert i (Some expr) b))
