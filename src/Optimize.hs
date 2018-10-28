08dd35f1fd065ca872db0c1ce388ca41bbf813ff
Ruud van Asseldonk <dev@veniogames.com>
2018-10-28 04:49:11 +0100

    Add optimizations for boolean expressions
    The optimizer can now already figure out that the condition for my test
    program is just "false". I did not even see that myself. I should change
    it to not be unconditionally false, otherwise the example is silly.

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -2,6 +2,8 @@ module Optimize
   ( optimize
   ) where
 
+import Data.Foldable (foldr')
+
 import Types (Expr (..), Deref (..), Tag (..), Variable, Value, mapBindings, mapExpr)
 import Program (Gen, Program (..))
 
@@ -26,11 +28,19 @@ pattern DConst value = DefExpr (Const value)
 pattern DNot :: t Bool -> Deref t Bool
 pattern DNot expr = DefExpr (Not expr)
 
+pattern DAnd :: [t Bool] -> Deref t Bool
+pattern DAnd expr = DefExpr (And expr)
+
+pattern DOr :: [t Bool] -> Deref t Bool
+pattern DOr expr = DefExpr (Or expr)
+
 pattern VTrue :: Value Bool
 pattern VTrue = TagBool True
 
 pattern VFalse :: Value Bool
 pattern VFalse = TagBool False
+
+type DoDeref = forall a. Variable a -> Deref Variable a
 
 optimize :: Program a b -> Program a b
 optimize program =
@@ -45,17 +55,43 @@ optimize program =
     program { programBindings = newBindings }
 
 -- Apply rewrite rules that eliminate operations.
-rewriteExpr :: (forall a. Variable a -> Deref Variable a) -> Expr Variable b -> Expr Variable b
+rewriteExpr :: DoDeref -> Expr Variable b -> Expr Variable b
 rewriteExpr deref expr = case expr of
   Not (deref -> DConst VTrue) -> Const VFalse
   Not (deref -> DConst VFalse) -> Const VTrue
   Not (deref -> DNot x) -> Id x
+  And []  -> Const VTrue
+  And [x] -> Id x
+  And xs  -> rewriteAnd deref xs
+  Or []  -> Const VFalse
+  Or [x] -> Id x
+  Or xs  -> rewriteOr deref xs
   _ -> expr
+
+rewriteAnd :: DoDeref -> [Variable Bool] -> Expr Variable Bool
+rewriteAnd deref = foldr' f (And [])
+  where
+    f (deref -> DConst VFalse) _  = Const VFalse
+    f (deref -> DConst VTrue) z   = z
+    -- TODO: This next rule should only be applied if this is the only consumer.
+    f (deref -> DAnd ys) z = foldr' f z ys
+    f x (And xs) = And (x : xs)
+    f _ z        = z
+
+rewriteOr :: DoDeref -> [Variable Bool] -> Expr Variable Bool
+rewriteOr deref = foldr' f (Or [])
+  where
+    f (deref -> DConst VTrue) _  = Const VTrue
+    f (deref -> DConst VFalse) z = z
+    -- TODO: This next rule should only be applied if this is the only consumer.
+    f (deref -> DOr ys) z = foldr' f z ys
+    f x (Or xs)  = Or (x : xs)
+    f _ z        = z
 
 -- Replace references to an identity expression with references to the source of
 -- the identity expression. The replacement follows all the way through to the
 -- expression that is not an identity expression.
-eliminateId :: (forall a. Variable a -> Deref Variable a) -> Expr Variable b -> Expr Variable b
+eliminateId :: DoDeref -> Expr Variable b -> Expr Variable b
 eliminateId deref =
   let
     deepSource :: forall c. Variable c -> Variable c
