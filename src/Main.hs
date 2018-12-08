41b33a23ab85a9f71f9712109451693c3da164b8
Ruud van Asseldonk <dev@veniogames.com>
2018-12-08 15:08:01 +0100

    Add Apache 2.0 license

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,10 @@
+-- Dataflow Compiler
+-- Copyright 2018 Ruud van Asseldonk
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- A copy of the License has been included in the root of the repository.
+
 import Control.Monad (void)
 
 import Types (Tag (..), Field (..))
